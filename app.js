e788747bb4df53b8838607645d21c86d7c055a84
Abdulaziz <abdulazizamrullayev557@gmail.com>
2024-07-24 08:28:00 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,11 @@
+let servet=document.querySelector('#services');
+let round=document.querySelector('#round');
+
+servet.addEventListener('click',()=>{
+    round.classList.toggle('roundd');
+    round.classList.toggle('round');
+})
+
+servet.addEventListener('click',()=>{
+    console.log('fwefew');
+})
